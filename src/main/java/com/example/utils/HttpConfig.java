ed746e867dd2fddc6fb13123e600f0d522900362
zhouxiongxin <424468708@qq.com>
2019-12-26 16:50:28 +0800

    update

--- src/main/java/com/example/utils/HttpConfig.java
+++ src/main/java/com/example/utils/HttpConfig.java
@@ -2,8 +2,8 @@ package com.example.utils;
 
 public class HttpConfig {
     //阿里云环境
-    public static final  String url="platform.huasheng100.com/user-base";
-    public static final  Integer port=8080;
+    public static final String url="platform.huasheng100.com/user-base";
+    //public static final Integer port=8080;
     public static final String scheme="http";
 
     //测试环境
@@ -12,9 +12,9 @@ public class HttpConfig {
     public static final String priScheme="http";
 
     //Product-Service
-    public static final  String Serviceurl="172.18.0.149";
-    public static final  Integer Serviceport=80;
-    public static final String Servicescheme="http";
+    //public static final  String Serviceurl="172.18.0.149";
+    //public static final  Integer Serviceport=80;
+    //public static final String Servicescheme="http";
 
 
 
