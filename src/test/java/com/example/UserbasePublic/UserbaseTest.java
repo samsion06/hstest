4117fb378d54276cc48cb5b00875749d26a27308
zhouxiongxin <424468708@qq.com>
2019-12-30 17:50:04 +0800

    update

--- src/test/java/com/example/UserbasePublic/UserbaseTest.java
+++ src/test/java/com/example/UserbasePublic/UserbaseTest.java
@@ -12,9 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import java.io.IOException;
 import java.net.URI;
 
